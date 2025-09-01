9380a06ab27c15b1c5a74c63a28783faa1481eee
shailendra chaturvedi <shailendrachaturvedi212@gmail.com>
2025-09-01 14:32:12 +0530

    added homecontroller and its views and viewmodels, added anchor tag and menu to navigate within the application

--- DemoShopApplication/Controllers/PieController.cs
+++ DemoShopApplication/Controllers/PieController.cs
@@ -21,8 +21,16 @@ namespace DemoShopApplication.Controllers
 
             //return View(_pieRepository.AllPies);
 
-            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "All Pies");
             return View(piesListViewModel);
+        }
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
         }
     }
 }
---
+++ DemoShopApplication/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DemoShopApplication.Models;
+
+namespace DemoShopApplication.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek;
+        }
+    }
+}
